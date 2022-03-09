4fb18b77328902c855c241976b0e8929563f632f
MicknotMitch <101023569+MicknotMitch@users.noreply.github.com>
2022-03-09 09:18:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('datashare:ready', ({ detail }) => {
         dspath=detail.core.router.app.$store._modulesNamespaceMap["document/"].state.doc._RAW._source.path;
         newroot="//stthomas";
         newpath=dspath.replace(dsroot,newroot);
-        return "<div class=\"text-center bg-info p-2 width-100\"><a href=\"file:"+newpath+"\">"+newpath+"</a>";
+        return "<div class=\"text-center bg-info p-2 width-100\"><a href=\"file:"+newpath+"\">"+newpath+"</a></div>";
       }
     },
     template: `<span v-html="docpath"></span>`
