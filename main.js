d70649f8e1452799aae6145179b3698b8d60b011
MicknotMitch <101023569+MicknotMitch@users.noreply.github.com>
2022-03-09 08:55:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('datashare:ready', ({ detail }) => {
+  const Alert = {
+    computed: {
+      docpath () {
+        dsroot=detail.core.router.app.$store._modulesNamespaceMap["document/"].state.doc._RAW._source.dirname;
+        dspath=detail.core.router.app.$store._modulesNamespaceMap["document/"].state.doc._RAW._source.path;
+        newroot="//stthomas";
+        newpath=dspath.replace(dsroot,newroot);
+        return "<div class=\"text-center bg-info p-2 width-100\"><a href=\"file:"+newpath+"\">"+newpath+"</a>";
+      }
+    },
+    template: `<span v-html="docpath"></span>`
+  }
+  detail.core.registerHook({ target: 'document.header:before', definition: Alert })
+})
