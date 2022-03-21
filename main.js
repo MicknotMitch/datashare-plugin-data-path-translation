285f436d643901fdf384a295d1c0a5e6f1dd9145
MicknotMitch <101023569+MicknotMitch@users.noreply.github.com>
2022-03-20 22:33:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ document.addEventListener('datashare:ready', ({ detail }) => {
       docpath () {
         dsroot=detail.core.router.app.$store._modulesNamespaceMap["document/"].state.doc._RAW._source.dirname;
         dspath=detail.core.router.app.$store._modulesNamespaceMap["document/"].state.doc._RAW._source.path;
-        newroot="//stthomas";
-        newpath=dspath.replace(dsroot,newroot);
+        newroot="//server";
+        newpath=dspath.replace(dsroot,newroot);        
         return "<div class=\"text-center bg-info p-2 width-100\"><a href=\"file:"+newpath+"\">"+newpath+"</a></div>";
+        // due to security concerns, browsers no longer allow you to use the "file://" protocol,
+        // you will either need to use a browser extentions that enables the file protocol or
+        // define a new protocol as described here (for windows) https://stackoverflow.com/questions/80650/how-do-i-register-a-custom-url-protocol-in-windows
+        // I believe this can be done in linux using .desktop files
       }
     },
     template: `<span v-html="docpath"></span>`
